5d8585639675ec78f5a8c31495846d3029cb30d2
epmaur <eppmauring@gmail.com>
2017-03-03 18:11:21 +0200

    app failid

---
+++ adPortal/src/app.js
@@ -0,0 +1,14 @@
+export class App {
+configureRouter(config, router) {
+    this.router = router;
+    config.title = 'My Aurelia äpp';
+
+    config.map([
+      { route: ['', 'home'],       name: 'home',       moduleId: 'home/index' },
+      { route: 'kaotatud_kettad', name: 'kaotatud_kettad',  moduleId: './kaotatud_kettad/kaotatud_kettad',   nav: true },
+      { route: 'leitud_kettad', name: 'leitud_kettad',  moduleId: './leitud_kettad/leitud_kettad',   nav: true },
+      { route: 'minu_kuulutused', name: 'minu_kuulutused',  moduleId: './minu_kuulutused/minu_kuulutused',   nav: true },
+      { route: 'uus_kuulutus', name: 'uus_kuulutus',  moduleId: './uus_kuulutus/uus_kuulutus',   nav: true }
+    ]);
+  }
+}
